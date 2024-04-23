27184054eeb0a75828a55fba2001ea3d57b010ba
Ourai Lin <ourairyu@gmail.com>
2024-04-23 22:56:46 +0800

    fix(script): copy package metadata before generate site

--- .knosys/scripts/command/deploy.js
+++ .knosys/scripts/command/deploy.js
@@ -2,7 +2,7 @@ const { resolve: resolvePath } = require('path');
 const { existsSync } = require('fs');
 const { generateHexoSite } = require('@knosys/sdk/src/site/generators/hexo');
 
-const { resolveRootPath, resolveSiteSrcDir, execute } = require('../helper');
+const { resolveRootPath, resolveSiteSrcDir, copySitePkgInfo, execute } = require('../helper');
 
 module.exports = {
   execute: distDir => {
@@ -10,6 +10,7 @@ module.exports = {
       const distPath = resolvePath(resolveRootPath(), distDir);
 
       if (existsSync(distPath)) {
+        copySitePkgInfo(site);
         generateHexoSite(resolveSiteSrcDir(), distPath);
       } else {
         console.log(`[ERROR] 路径 \`${distPath}\` 不存在`);
--- .knosys/scripts/command/serve.js
+++ .knosys/scripts/command/serve.js
@@ -1,15 +1,8 @@
-const { resolve: resolvePath } = require('path');
-const { pick } = require('@ntks/toolbox');
-
-const { resolveRootPath, readData, saveData, execute, resolveSiteSrcDir } = require('../helper');
+const { copySitePkgInfo, execute } = require('../helper');
 
 module.exports = {
   execute: (site = 'default') => {
-    const rootPath = resolveRootPath();
-    const pkg = readData(`${rootPath}/package.json`);
-    const siteSrcPath = resolvePath(rootPath, resolveSiteSrcDir(site));
-
-    saveData(`${siteSrcPath}/package.json`, { name: `${pkg.name}-site-${site}`, ...pick(pkg, ['version', 'private', 'hexo', 'dependencies']) });
+    copySitePkgInfo(site);
     execute('site', 'serve', site);
   },
 };
